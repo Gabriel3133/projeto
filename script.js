bca5961dbd0e0b4515a40ae96d2839435b781f24
Gabriel3133 <gabrielvb379@gmail.comgit config --global user.email gabrielvb379@gmail.com>
2025-03-25 09:46:30 -0300

    add commit

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-function toggleMode () {
-    //alert ("elisa e feia")
-   const html = document.documentElement
-   html.classList.toggle('light')
-     
-    const img = document.querySelector ("#profile img")
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
 
-   if (html.classList.contains("light")){
-        img.setAttribute ('src', "./assets/Avatar3.png")
-        img.setAttribute ('alt', 'Avatar 3')
-    } 
-    else {
-        img.setAttribute ('src', "./assets/avatar.png")
-        img.setAttribute ('alt', 'Avatar')
+  const img = document.querySelector("#profile img")
 
-        
-    }
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/Avatar3.png")
+    img.setAttribute("alt", "Avatar 3")
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("alt", "Avatar")
+  }
 }
